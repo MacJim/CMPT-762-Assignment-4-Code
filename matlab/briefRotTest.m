15bc1762c7e1559da087f83b9ac263f0e0fc7468
Jim Shi <shiyilin2008@gmail.com>
2021-03-23 16:34:02 -0700

    Adds BRIEF rotation test script.

--- matlab/briefRotTest.m
+++ matlab/briefRotTest.m
@@ -1,17 +1,36 @@
-% Your solution to Q2.1.5 goes here!
+% Step 4.2: BRIEF and Rotations
+% This script only contains BRIEF. Refer to the `surfRotTest.m` script for SURF.
 
-%% Read the image and convert to grayscale, if necessary
-
-%% Compute the features and descriptors
-
-for i = 0:36
-    %% Rotate image
-    
-    %% Compute features and descriptors
-    
-    %% Match features
-    
-    %% Update histogram
+% MARK: Read the image and convert to grayscale, if necessary
+img = imread('../data/cv_cover.jpg');
+if (ndims(img) == 3)
+    img = rgb2gray(img);
 end
 
-%% Display histogram
+
+% MARK: BRIEF
+briefMatches = [];
+
+for i = 0:36
+    % Rotate image.
+    rotated_img = imrotate(img, i * 10);
+    
+    % Match features.
+    [locs1, locs2] = matchPics(img, rotated_img);
+    
+    % Plot feature matching result at three different orientations.
+    if ((i == 3) || (i == 6) || (i == 9))
+        figure('Name', sprintf('BRIEF: Rotation: %d Degrees', i * 10), 'NumberTitle', 'off');
+        showMatchedFeatures(img, rotated_img, locs1, locs2, 'montage');
+    end
+    
+    % Update histogram count.
+    briefMatches = [briefMatches size(locs1, 1)];
+end
+
+% Display histogram.
+figure;
+bar(0:10:360, briefMatches);
+xlabel('Rotation in Degrees');
+ylabel('Matched Feature Count');
+title('BRIEF Matched Features Count');
