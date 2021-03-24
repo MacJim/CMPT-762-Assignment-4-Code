f9af09cabc4894b3f6e9d58d5efa96b26ce3abe1
Jim Shi <shiyilin2008@gmail.com>
2021-03-23 19:05:29 -0700

    Adds the homography matrix computation and visualization scripts.

--- matlab/computeH.m
+++ matlab/computeH.m
@@ -1,3 +1,22 @@
-function [ H2to1 ] = computeH( x1, x2 )
-%COMPUTEH Computes the homography between two sets of points
+% Step 4.3: Homography Computation
+
+function [H2to1] = computeH(x1, x2)
+% Computes the homography between two sets of points.
+% x1, x2: N x 2 matrices of (x, y) points.
+% H2to1: 3 x 3 matrix of the best homography from image 2 to image 1 in the least-square sense.
+
+% disp(size(x1))
+
+A = zeros(2 * size(x1, 1), 9);
+
+for i = 1:size(x1, 1)
+    A(2 * i - 1, :) = [-x2(i, 1), -x2(i, 2), -1, 0, 0, 0, x2(i, 1) * x1(i, 1), x1(i, 1) * x2(i, 2), x1(i, 1)];
+    A(2 * i, :) = [0, 0, 0, -x2(i, 1), -x2(i, 2), -1, x1(i, 2) * x2(i, 1), x1(i, 2) * x2(i, 2), x1(i, 2)];
+end
+
+[~, ~, V] = svd(A);
+
+H2to1 = V(:, 9);
+H2to1 = reshape(H2to1, [3, 3])';
+
 end
---
+++ matlab/visualizeH.m
@@ -0,0 +1,28 @@
+% Visualize Homography 2 to 1.
+
+coverImg = imread('../data/cv_cover.jpg');
+deskImg = imread('../data/cv_desk.png');
+
+
+% MARK: Match
+[locs1, locs2] = matchPics(coverImg, deskImg);
+figure('Name', 'Matched Features', 'NumberTitle', 'off');
+showMatchedFeatures(coverImg, deskImg, locs1, locs2, 'montage');    % There are some outliers on the black spots of the desk.
+
+
+% MARK: computeH
+H2to1 = computeH(locs1, locs2);
+
+% Randomly select 10 points
+% Source: https://www.mathworks.com/matlabcentral/answers/35258-randomly-select-elements-of-an-array
+idx = randperm(size(locs1, 1), 10);
+
+correspondingLocs1 = [];
+for i = 1:10
+    point2 = [locs2(idx(i),:)'; 1];    % Append 1.
+    pointArr = H2to1 * point2;
+    correspondingLocs1 = [correspondingLocs1; (pointArr / pointArr(3))'];    % Divide by final element here.
+end
+
+figure('Name', 'computeH', 'NumberTitle', 'off');
+showMatchedFeatures(coverImg, deskImg, correspondingLocs1(:,1:2), locs2(idx, :), 'montage');
