2a00f6f414d66ae1e0a42a75b1b4a3fa7a9ce2ce
Jim Shi <shiyilin2008@gmail.com>
2021-03-24 01:22:26 -0700

    Adds the normalized homography calculation function.

--- matlab/computeH_norm.m
+++ matlab/computeH_norm.m
@@ -1,20 +1,40 @@
+% Step 4.4: normalized homography.
+
 function [H2to1] = computeH_norm(x1, x2)
 
-%% Compute centroids of the points
-centroid1 = ;
-centroid2 = ;
+%% Compute centroids of the points.
+centroid1 = mean(x1);
+centroid2 = mean(x2);
 
-%% Shift the origin of the points to the centroid
+%% Shift the origin of the points to the centroid.
+x1 = x1 - centroid1;
+x2 = x2 - centroid2;
 
 %% Normalize the points so that the average distance from the origin is equal to sqrt(2).
+averageDistance1 = mean(hypot(x1(:, 1), x1(:, 2)));
+averageDistance2 = mean(hypot(x2(:, 1), x2(:, 2)));
+scale1 = sqrt(2) / averageDistance1;
+scale2 = sqrt(2) / averageDistance2;
 
-%% similarity transform 1
-T1 = [];
+x1 = x1 * scale1;
+x2 = x2 * scale2;
 
-%% similarity transform 2
-T2 = [];
+%% similarity transform 1.
+T1 = [
+    scale1, 0, -centroid1(1) * scale1;
+    0, scale1, -centroid1(2) * scale1;
+    0, 0, 1;
+];
 
-%% Compute Homography
+%% similarity transform 2.
+T2 = [
+    scale2, 0, -centroid2(1) * scale2;
+    0, scale2, -centroid2(2) * scale2;
+    0, 0, 1;
+];
 
-%% Denormalization
-H2to1 = ;
+%% Compute Homography.
+H = computeH(x1, x2);
+
+%% Denormalization.
+H2to1 = T1 \ H * T2;
--- matlab/visualizeH.m
+++ matlab/visualizeH.m
@@ -26,3 +26,21 @@ end
 
 figure('Name', 'computeH', 'NumberTitle', 'off');
 showMatchedFeatures(coverImg, deskImg, correspondingLocs1(:,1:2), locs2(idx, :), 'montage');
+
+
+% MARK: computeH_norm
+H2to1 = computeH_norm(locs1, locs2);
+
+% Randomly select 10 points
+% Source: https://www.mathworks.com/matlabcentral/answers/35258-randomly-select-elements-of-an-array
+idx = randperm(size(locs1, 1), 10);
+
+correspondingLocs1 = [];
+for i = 1:10
+    point2 = [locs2(idx(i),:)'; 1];    % Append 1.
+    pointArr = H2to1 * point2;
+    correspondingLocs1 = [correspondingLocs1; (pointArr / pointArr(3))'];    % Divide by final element here.
+end
+
+figure('Name', 'computeH_norm', 'NumberTitle', 'off');
+showMatchedFeatures(coverImg, deskImg, correspondingLocs1(:,1:2), locs2(idx, :), 'montage');
