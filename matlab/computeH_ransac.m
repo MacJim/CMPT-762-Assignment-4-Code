a38122b424e906e6ea79bfc90d3db13c2c66ea04
Jim Shi <shiyilin2008@gmail.com>
2021-03-24 17:31:03 -0700

    Homography calculation: Adds RANSAC support.

--- matlab/computeH_ransac.m
+++ matlab/computeH_ransac.m
@@ -1,7 +1,49 @@
-function [ bestH2to1, inliers] = computeH_ransac( locs1, locs2)
-%COMPUTEH_RANSAC A method to compute the best fitting homography given a
-%list of matching points.
+% Step 4.5: homography with RANSAC.
+%
+% Minimum requirement: 4 point pairs
 
-%Q2.2.3
+function [bestH2to1, inliers, bestIdx] = computeH_ransac(locs1, locs2)
+% COMPUTEH_RANSAC A method to compute the best fitting homography given a list of matching points.
+% bestH2to1: Homography H with most inliers.
+% inliers: A vector of length N with a 1 at those matches that are part of the consensus set, and 0 elsewhere.
+% bestIdx: Indices of points used to compute `bestH2to1`.
+
+inlierThreshold = 10;
+
+bestH2to1 = [];
+inliers = [];
+bestIdx = [];
+
+maxInliersCount = 0;
+
+for i = 1:1000
+    % Choose 4 point pairs.
+    idx = randperm(size(locs1, 1), 4);
+
+    % Compute H.
+    H2to1 = computeH_norm(locs1(idx, :), locs2(idx, :));
+    
+    % Find current inliers.
+    currentInliers = zeros(size(locs1, 1), 1);
+    for j = 1:size(locs1, 1)
+        targetPoint = H2to1 * [locs2(j, :)'; 1];    % Append 1.
+        targetPoint = targetPoint / targetPoint(3);
+        loss = sqrt((targetPoint(1) - locs1(j, 1))^2 + (targetPoint(2) - locs1(j, 2))^2);
+        if (loss < inlierThreshold)
+            currentInliers(j) = 1;
+        end
+    end
+
+    % Update return value if this H yields more inliers.
+    currentInliersCount = length(find(currentInliers == 1));
+    if (currentInliersCount > maxInliersCount)
+        bestH2to1 = H2to1;
+        inliers = currentInliers;
+        bestIdx = idx;
+
+        maxInliersCount = currentInliersCount;
+    end
+end
+
 end
 
--- matlab/visualizeH.m
+++ matlab/visualizeH.m
@@ -44,3 +44,24 @@ end
 
 figure('Name', 'computeH_norm', 'NumberTitle', 'off');
 showMatchedFeatures(coverImg, deskImg, correspondingLocs1(:,1:2), locs2(idx, :), 'montage');
+
+
+% MARK: computeH_ransac
+[bestH2to1, inliers, bestIdx] = computeH_ransac(locs1, locs2);
+
+% Visualize the 4 point-pairs (that produced the most number of inliers).
+figure('Name', 'computeH_ransac Chosen Points', 'NumberTitle', 'off');
+showMatchedFeatures(coverImg, deskImg, locs1(bestIdx, :), locs2(bestIdx, :), 'montage');
+
+% Visualize the inlier matches that were selected by RANSAC.
+correspondingLocs1 = [];
+inliersIdx = find(inliers == 1);
+inliersLocs2 = locs2(inliersIdx, :);
+for i = 1:length(inliersLocs2)
+    point2 = [inliersLocs2(i, :)'; 1];
+    pointArr = bestH2to1 * point2;
+    correspondingLocs1 = [correspondingLocs1; (pointArr / pointArr(3))'];
+end
+
+figure('Name', 'computeH_ransac Inliers', 'NumberTitle', 'off');
+showMatchedFeatures(coverImg, deskImg, correspondingLocs1(:, 1:2), inliersLocs2, 'montage');
